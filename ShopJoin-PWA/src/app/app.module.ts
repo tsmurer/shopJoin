fba8037a6987a0a7ae34d739898671163faa4962
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-06-22 18:47:35 -0300

    setting styles to scss, starting navigation component and setting some visual elements

--- ShopJoin-PWA/src/app/app.module.ts
+++ ShopJoin-PWA/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      UserComponent
+      UserComponent,
+      NavigationComponent
    ],
    imports: [
       BrowserModule,
