7e71d10ed365d329a1bfd6819d1fa724121890e7
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-07-03 15:01:31 -0300

    dis sunvabitch workin now

--- ShopJoin-PWA/src/app/mdc.module.ts
+++ ShopJoin-PWA/src/app/mdc.module.ts
@@ -1,45 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-  MdcButtonModule,
-  MdcCardModule,
-  MdcCheckboxModule,
-  MdcChipsModule,
-  MdcDialogModule,
-  MdcDrawerModule,
-  MdcElevationModule,
-  MdcFabModule,
-  MdcFormFieldModule,
-  MdcIconModule,
-  MdcLinearProgressModule,
-  MdcListModule,
-  MdcMenuModule,
-  MdcRadioModule,
-  MdcRippleModule,
-  MdcSelectModule,
-  MdcSliderModule,
-  MdcSnackbarModule,
-  MdcSwitchModule,
-  MdcTabModule,
-  MdcTextFieldModule,
-  MdcTypographyModule,
-} from '@angular-mdc/web/';
+import {MdcButtonModule} from '@angular-mdc/web/button';
+import {MdcCardModule} from '@angular-mdc/web/card';
+import {MdcCheckboxModule} from '@angular-mdc/web/checkbox';
+import {MdcChipsModule} from '@angular-mdc/web/chips';
+import {MDCDataTableModule} from '@angular-mdc/web/data-table';
+import {MdcDialogModule} from '@angular-mdc/web/dialog';
+import {MdcDrawerModule} from '@angular-mdc/web/drawer';
+import {MdcFabModule} from '@angular-mdc/web/fab';
+import {MdcFormFieldModule} from '@angular-mdc/web/form-field';
+import {MdcIconModule} from '@angular-mdc/web/icon';
+import {MdcIconButtonModule} from '@angular-mdc/web/icon-button';
+import {MdcImageListModule} from '@angular-mdc/web/image-list';
+import {MdcLinearProgressModule} from '@angular-mdc/web/linear-progress';
+import {MdcListModule} from '@angular-mdc/web/list';
+import {MdcMenuModule} from '@angular-mdc/web/menu';
+import {MdcMenuSurfaceModule} from '@angular-mdc/web/menu-surface';
+import {MdcRadioModule} from '@angular-mdc/web/radio';
+import {MdcSelectModule} from '@angular-mdc/web/select';
+import {MdcSliderModule} from '@angular-mdc/web/slider';
+import {MdcSnackbarModule} from '@angular-mdc/web/snackbar';
+import {MdcSwitchModule} from '@angular-mdc/web/switch';
+import {MdcTabBarModule} from '@angular-mdc/web/tab-bar';
+import {MdcTextFieldModule} from '@angular-mdc/web/textfield';
+import {MdcTopAppBarModule} from '@angular-mdc/web/top-app-bar';
+
+import {MdcElevationModule} from '@angular-mdc/web/elevation';
+import {MdcRippleModule} from '@angular-mdc/web/ripple';
+import {MdcTypographyModule} from '@angular-mdc/web/typography';
 
 
 @NgModule({
   imports: [
+
     CommonModule,
     MdcButtonModule,
     MdcCardModule,
     MdcCheckboxModule,
     MdcChipsModule,
+    MDCDataTableModule,
     MdcDialogModule,
     MdcDrawerModule,
     MdcElevationModule,
     MdcFabModule,
     MdcFormFieldModule,
     MdcIconModule,
+    MdcIconButtonModule,
+    MdcImageListModule,
     MdcLinearProgressModule,
     MdcListModule,
     MdcMenuModule,
@@ -49,12 +57,38 @@ import {
     MdcSliderModule,
     MdcSnackbarModule,
     MdcSwitchModule,
-    MdcTabModule,
+    MdcTabBarModule,
     MdcTextFieldModule,
+    MdcTopAppBarModule,
     MdcTypographyModule,
   ],
   exports: [
-    MdcCardModule
+    MdcButtonModule,
+    MdcCardModule,
+    MdcCheckboxModule,
+    MdcChipsModule,
+    MDCDataTableModule,
+    MdcDialogModule,
+    MdcDrawerModule,
+    MdcElevationModule,
+    MdcFabModule,
+    MdcFormFieldModule,
+    MdcIconModule,
+    MdcIconButtonModule,
+    MdcImageListModule,
+    MdcLinearProgressModule,
+    MdcListModule,
+    MdcMenuModule,
+    MdcRadioModule,
+    MdcRippleModule,
+    MdcSelectModule,
+    MdcSliderModule,
+    MdcSnackbarModule,
+    MdcSwitchModule,
+    MdcTabBarModule,
+    MdcTextFieldModule,
+    MdcTopAppBarModule,
+    MdcTypographyModule,
   ],
   providers: [
   ],
